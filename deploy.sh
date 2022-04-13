15dd0284407c03f4fecfdf386d5ebf35ceeb8e27
dmitry <gfnhbrqpdtplf@bk.ru>
2022-04-13 22:41:13 +0500

    added files

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка приложения
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# инициализация репозитория и загрузка кода в GitHub
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:dimitry-blinov.github.io.git master
+
+git push -f git@github.com:dimitry-blinov/Vue-project-shop-moire.git master:gh-pages
+
+cd -
