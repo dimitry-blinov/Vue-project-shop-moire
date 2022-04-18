300d46584e3463931f12779f5659c9fa0bb48483
dmitry <gfnhbrqpdtplf@bk.ru>
2022-04-18 14:26:07 +0500

    added filtering by color, season, material

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,262 +7,135 @@
           Каталог
         </h1>
         <span class="content__info">
-          152 товара
+          {{ countProducts }} {{ stringProduct(countProducts) }}
         </span>
       </div>
     </div>
 
     <div class="content__catalog">
 
-      <!--
-      <aside class="filter">
-        <form class="filter__form form" action="#" method="get">
-          <fieldset class="form__block">
-            <legend class="form__legend">Цена</legend>
-            <label class="form__label form__label--price">
-              <input class="form__input" type="text" name="min-price" value="0">
-              <span class="form__value">От</span>
-            </label>
-            <label class="form__label form__label--price">
-              <input class="form__input" type="text" name="max-price" value="12345">
-              <span class="form__value">До</span>
-            </label>
-          </fieldset>
-
-          <fieldset class="form__block">
-            <legend class="form__legend">Категория</legend>
-            <label class="form__label form__label--select">
-              <select class="form__select" name="category">
-                <option value="value1">Все категории</option>
-                <option value="value2">Футболки</option>
-                <option value="value3">Бюстгалтеры</option>
-                <option value="value4">Носки</option>
-              </select>
-            </label>
-          </fieldset>
-
-          <fieldset class="form__block">
-            <legend class="form__legend">Материал</legend>
-            <ul class="check-list">
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="material"
-                         value="лен">
-                  <span class="check-list__desc">
-                    лен
-                    <span>(3)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="material"
-                         value="хлопок">
-                  <span class="check-list__desc">
-                    хлопок
-                    <span>(46)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="material"
-                         value="шерсть">
-                  <span class="check-list__desc">
-                    шерсть
-                    <span>(20)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="material"
-                         value="шелк">
-                  <span class="check-list__desc">
-                    шелк
-                    <span>(30)</span>
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </fieldset>
-
-          <fieldset class="form__block">
-            <legend class="form__legend">Коллекция</legend>
-            <ul class="check-list">
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="collection"
-                         value="лето" checked="">
-                  <span class="check-list__desc">
-                    лето
-                    <span>(2)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="collection"
-                         value="зима">
-                  <span class="check-list__desc">
-                    зима
-                    <span>(53)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="collection"
-                         value="весна">
-                  <span class="check-list__desc">
-                    весна
-                    <span>(24)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="collection"
-                         value="осень">
-                  <span class="check-list__desc">
-                    осень
-                    <span>(30)</span>
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </fieldset>
-
-          <button class="filter__submit button button--primery" type="submit">
-            Применить
-          </button>
-          <button class="filter__reset button button--second" type="button">
-            Сбросить
-          </button>
-        </form>
-      </aside>
--->
+      <product-filter :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo"
+                      :category-id.sync="filterCategoryId" :color.sync="filterColor"
+                      :page.sync="page" :material.sync="filterMaterial"
+                      :season.sync="filterSeason"/>
       <section class="catalog">
 
-        <product-list :products="products"/>
+        <div v-if="products.length === 0 && !productsLoading">
+          <h4>
+            Товаров, соответствующих вашему запросу, не обнаружено
+          </h4>
+        </div>
 
-        <base-pagination/>
-        <!--
-        <ul class="catalog__pagination pagination">
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow pagination__link--disabled"
-               aria-label="Предыдущая страница">
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-left"></use>
-              </svg>
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--current">
-              1
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              2
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              3
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              4
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              ...
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              10
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow" href="#"
-               aria-label="Следующая страница">
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-right"></use>
-              </svg>
-            </a>
-          </li>
-        </ul>
-        -->
+        <base-loader loader-title="Загрузка товаров" v-if="productsLoading"/>
+        <product-list :products="products" v-else/>
+
+        <base-pagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
+
       </section>
     </div>
   </main>
 </template>
 
 <script>
-import stringProduct from '@/helpers/stringProduct';
-// eslint-disable-next-line import/extensions
-import BasePagination from '@/components/BasePagination';
-// eslint-disable-next-line import/extensions
-import ProductList from '@/components/ProductList';
-import axios from 'axios';
-// eslint-disable-next-line import/no-duplicates
-import { API_BASE_URL, NO_IMAGE } from '@/config';
+import stringProduct from '@/helpers/stringProduct'
+import BaseLoader from '@/components/BaseLoader'
+import ProductFilter from '@/components/ProductFilter'
+import BasePagination from '@/components/BasePagination'
+import ProductList from '@/components/ProductList'
+import axios from 'axios'
+import { API_BASE_URL, NO_IMAGE } from '@/config'
 
 export default {
   name: 'MainView',
-  data() {
+  data () {
     return {
       page: 1,
-      productsPerPage: 3,
+      productsPerPage: 6,
       filterPriceFrom: 0,
       filterPriceTo: 0,
       filterCategoryId: 0,
-      filterColor: 0,
+      filterColor: [],
+      filterMaterial: [],
+      filterSeason: [],
       stringProduct,
 
       productsLoading: false,
       productsLoadingFailed: false,
 
-      productsData: null,
-    };
+      productsData: null
+    }
   },
   components: {
     BasePagination,
     ProductList,
+    ProductFilter,
+    BaseLoader
   },
   computed: {
-    products() {
-      return this.productsData ? this.productsData.map((product) => ({
-        ...product,
-        image: product.colors[0].gallery ? product?.colors[0]?.gallery[0].file.url : `${NO_IMAGE}`,
-      }))
-        : [];
+    products () {
+      return this.productsData
+        ? this.productsData.items.map((product) => ({
+          ...product,
+          image: product.colors[0].gallery
+            ? product?.colors[0]?.gallery[0].file.url
+            : `${NO_IMAGE}`
+        }))
+        : []
     },
+    countProducts () {
+      return this.productsData ? this.productsData.pagination.total : 0
+    }
   },
   methods: {
-    async loadProducts() {
+    async loadProducts () {
+      this.productsLoading = true
       const response = await axios(`${API_BASE_URL}/api/products`, {
         method: 'GET',
         params: {
           page: this.page,
           limit: this.productsPerPage,
           categoryId: this.filterCategoryId,
-          colorId: this.filterColor,
+          materialIds: this.filterMaterial,
+          seasonIds: this.filterSeason,
+          colorIds: this.filterColor,
           minPrice: this.filterPriceFrom,
-          maxPrice: this.filterPriceTo,
-        },
-      });
-      this.productsData = await response.data.items;
+          maxPrice: this.filterPriceTo
+        }
+      })
+      clearTimeout(this.loadProductsTimer)
+      this.loadProductsTimer = setTimeout(() => {
+        this.productsData = response.data
+        this.productsLoading = false
+      }, 1000)
+    }
+  },
+  watch: {
+    page () {
+      this.loadProducts()
     },
+    filterPriceFrom () {
+      this.loadProducts()
+    },
+    filterPriceTo () {
+      this.loadProducts()
+    },
+    filterCategoryId () {
+      this.loadProducts()
+    },
+    filterSeason () {
+      this.loadProducts()
+    },
+    filterMaterial () {
+      this.loadProducts()
+    },
+    filterColor () {
+      this.loadProducts()
+    }
   },
-  created() {
-    this.loadProducts();
-  },
-};
+  created () {
+    this.loadProducts()
+  }
+}
 </script>
 
 <style scoped>
