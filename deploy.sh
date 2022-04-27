4fb40ff85c4af6b80649355afeb6adb741200b01
dmitry <gfnhbrqpdtplf@bk.ru>
2022-04-27 15:02:20 +0500

    changed the parameters for deploy

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:dimitry-blinov.github.io.git master
-
 git push -f git@github.com:dimitry-blinov/Vue-project-shop-moire.git master:gh-pages
 
 cd -
